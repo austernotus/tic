d11954d2f72c6c2355320c136ab506b875d0a267
austernotus <76696499+austernotus@users.noreply.github.com>
2024-06-13 11:20:56 +0100

    figuring out gameboard and rounds, created players with marks

--- scripts.js
+++ scripts.js
@@ -1,11 +1,36 @@
-const gameboard = function gameboard(){
+let playerTurn = 1;
 
+const gameboard = (function (){
+    const setGameboard = () => 
+        ["","","",
+         "","","",
+         "","",""
+        ];
+    
+    return {setGameboard};
+})();
+
+function newPlayer(name, mark){
+    return {name, mark};
 }
 
-const player = function newPlayer(){
+const playRound = function (){
 
+    const placeMark = () => {
+        if(playerTurn === 1){
+            console.log(player1.mark);
+            playerTurn = 2;
+        } else{
+            console.log(player2.mark);
+            playerTurn = 1;
+        }
+
+    };
+    
+    return {placeMark};
 }
 
-const playRound = function playRound(){
 
-}
+const player1 = newPlayer("Player 1", "X")
+const player2 = newPlayer("Player 2", "O")
+const round = playRound();
